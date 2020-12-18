2918467198741952f45a3b38af317626342a5513
Huy Nguyen <huynn109@gmail.com>
2020-12-18 19:19:32 +0700

    link

--- src/2DArrayDS.kt
+++ src/2DArrayDS.kt
@@ -1,17 +1,18 @@
 import java.util.*
 
 /*
-@see <a href="https://www.hackerrank.com/challenges/2d-array">
+ * https://www.hackerrank.com/challenges/2d-array
  */
-// Complete the hourglassSum function below.
+
 fun hourglassSum(arr: Array<Array<Int>>): Int {
     var max = 0
     var first = true
     for (i in 1 until arr.size - 1) {
         for (j in 1 until arr[i].size - 1) {
-            if (first){
+            if (first) {
                 first = false
-                max = arr[i - 1][j - 1] + arr[i - 1][j] + arr[i - 1][j + 1] + arr[i][j] + arr[i + 1][j - 1] + arr[i + 1][j] + arr[i + 1][j + 1]
+                max =
+                    arr[i - 1][j - 1] + arr[i - 1][j] + arr[i - 1][j + 1] + arr[i][j] + arr[i + 1][j - 1] + arr[i + 1][j] + arr[i + 1][j + 1]
             }
             if (arr[i - 1][j - 1] + arr[i - 1][j] + arr[i - 1][j + 1] + arr[i][j] + arr[i + 1][j - 1] + arr[i + 1][j] + arr[i + 1][j + 1] > max)
                 max =
@@ -24,7 +25,7 @@ fun hourglassSum(arr: Array<Array<Int>>): Int {
 fun main(args: Array<String>) {
     val scan = Scanner(System.`in`)
 
-    val arr = Array<Array<Int>>(6, { Array<Int>(6, { 0 }) })
+    val arr = Array(6) { Array<Int>(6) { 0 } }
 
     for (i in 0 until 6) {
         arr[i] = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
